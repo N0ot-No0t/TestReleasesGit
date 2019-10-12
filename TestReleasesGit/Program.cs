a6db1fad108efd369afc740eb7f23beb279280ed
Cosmin Gabriel Sustac <sustaccosmin544@hotmail.ca>
2019-10-12 12:36:36 -0400

    Add project files.

---
+++ TestReleasesGit/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestReleasesGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Argument: "+args[0]);
+            Console.ReadKey();
+        }
+    }
+}
